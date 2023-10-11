8e89ce35854a696955894f06bb0554996c7a684c
Ian Ellis <ellisiana@gmail.com>
2023-10-11 12:40:00 +0100

    missing $ before common name var; inspect root cert

--- bin/build_ca_in_vault.sh
+++ bin/build_ca_in_vault.sh
@@ -2,10 +2,14 @@
 
 source ./config.sh
 
+# Create an offline Root CA (asks for passphrase entry)
 certstrap init \
      --organization "${CA_ORG}" \
      --organizational-unit "${CA_ORG_UNIT}" \
      --country "${CA_COUNTRY}" \
      --province "${CA_PROVINCE}" \
      --locality "${CA_LOCALITY}" \
-     --common-name "CA_COMMON_NAME"
+     --common-name "$CA_COMMON_NAME"
+
+# Inspect offline Root CA
+openssl x509 -in out/Testing_Root.crt -noout  -subject -issuer
