6f2a9356f76f67ac1265ff245b61acc19942a14b
Ian Ellis <ellisiana@gmail.com>
2023-10-12 13:48:22 +0100

    start vault server

--- bin/build_ca_in_vault.sh
+++ bin/build_ca_in_vault.sh
@@ -38,3 +38,55 @@ tree out
 #├── Testing_Root.crl
 #├── Testing_Root.crt
 #└── Testing_Root.key
+
+# Start Vault
+./bin/install-vault.sh
+
+# Wait for it to be running
+while true; do
+  # Run the kubectl command and capture the output
+  output=$(kubectl get all -n vault 2>&1)
+
+  # Check if the output contains the target status
+  if [[ $output =~ "Running" ]]; then
+    break
+  else
+    echo "Waiting for vault..."
+    sleep 5
+  fi
+done
+
+# Start port-forwarding in the background and keep it running
+while true; do
+  kubectl port-forward deployment.apps/vault -n vault --address 0.0.0.0 8200:8200 2>&1 >/dev/null
+
+  # Check the exit status of the previous command
+  if [ $? -eq 0 ]; then
+    echo "Port-forwarding is running successfully."
+  else
+    echo "Port-forwarding has exited with an error. Restarting..."
+    sleep 5  # Wait for a few seconds before restarting
+  fi
+done &
+
+export VAULT_ADDR=http://0.0.0.0:8200
+
+# Get running pod
+RUNNING_POD=$(kubectl get all -n vault|grep Running|cut -d\  -f1)
+
+# Wait for token to be available 
+while true; do
+  # Run the kubectl command and capture the output
+  output=$(kubectl logs ${RUNNING_POD} -n vault 2>&1)
+
+  # Check if the output contains the target status
+  if [[ $output =~ "Root Token" ]]; then
+    break
+  else
+    echo "Waiting for token..."
+    sleep 5
+  fi
+done
+
+export VAULT_TOKEN=$(kubectl logs $RUNNING_POD -n vault | grep "Root Token" | cut -d: -f2)
+
